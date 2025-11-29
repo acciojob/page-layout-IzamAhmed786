cdf1dae40614d8260b006eff1ed548c86199d06c
Your Name <your-email@example.com>
2025-11-29 11:36:20 +0530

    hello

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,9 @@ const App = () => {
   return (
     <div>
         {/* Do not remove the main div */}
+        <head>Welcome to my website</head>
+        <p>This is the content of my web site</p>
+        <footer>2023 My Website. All rights reserved</footer>
     </div>
   )
 }
